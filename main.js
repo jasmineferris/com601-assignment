f439ec68a5b3a1bf1fded2600549d38a398a8a35
Jasmine Ferris <jasmine.ferris.48@gmail.com>
2019-12-03 19:13:33 +0000

    updated tooltip

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
+//---------ADDRESS FORM SCRIPT FILE--------//
+//-----------------------------------------//
+
+
 //------TOOL TIP------//
 //--------------------//
-/*$(document).ready(function() {
-	xOffset = 20; //Position of tooltip on cursor
-	yOffset = 10;
-
-function tooltip(){ //from jQuery examples
-	$("input.tooltip").hover(function(e){
-		$("body").append("<p id='myTooltip'>Enter any details here that you want to search for <br> in your address book</p>"); //Appends p tag when hovering over the input field
-		},
-		function(){
-        	$("#myTooltip").remove(); //Tooltip not shown when not being hovered over
-		});
-		
-		$("input.tooltip").mousemove(function(e){
-			$("#myTooltip")
-            .css("top",(e.pageY - xOffset) + "px")
-            .css("left",(e.pageX + yOffset) + "px");
-		});
-	}
-        
-//TOOL TIP FUNCTION//
-$(document).ready(function(){ //Calls tooltip function
-    tooltip();
-	});
-});
-//---------------------------------//*/
 
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip({html:true});
+    $('[data-toggle="tooltip"]').tooltip({html:true}); //tooltip function. Use data-toggle to refer to the HTML file
 });
 
-
+//----------------------//
