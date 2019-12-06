cbe928a5090bc8856ca2e9dcac133477822b87ff
B00716443 <kelly-l53@ulster.ac.uk>
2019-12-06 12:52:22 +0000

    Gender field, removed DOB field.
    Index.js now generates a random gender (man, women or other). Removed the DOB field as the dates generated in Faker have time as well.
    
    Co-Authored-By: Jasmine Ferris <jasmineferris@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 // Uses JSON Server module
-
-
 const faker = require('faker');
 const fs = require('fs');
+
+    let genders = [ 'Male' , 'Female', 'Other' ];
 
 function generateContacts(){
     let users = [];
     for (let id =1; id <=50; id++){
         let firstName = faker.name.firstName();
         let lastName = faker.name.lastName();
+        let gender = faker.random.arrayElement(genders);
         let email = faker.internet.email();
         let address = faker.address.city();
-        let date_of_birth = faker.date.past();
         let city = faker.address.city();
         let post_code =faker.address.zipCode();
         let number = faker.phone.phoneNumber();
 
         users.push({
+            "id": id,
             "first_name": firstName,
             "last_name": lastName,
+            "gender" : gender,
             "email": email,
             "address": address,
-            "date_of_birth": date_of_birth,
             "city": city,
             "post_code": post_code,
             "number": number
