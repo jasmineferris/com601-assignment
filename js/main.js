d140738f9072e5754f6e7d69388485a36e620070
Jasmine Ferris <jasmine.ferris.48@gmail.com>
2019-12-09 14:07:51 +0000

    Alphabetical Sort
    Added button that allows the contact list to be sorted alphabetically by first name.

--- js/main.js
+++ js/main.js
@@ -83,3 +83,29 @@ function updateContact(){
 function deleteContact(){
 	hideMoreInfo();
 }
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table
+function sortTable(){
+	var table, rows, switching, i, x, y, shouldSwitch;
+  	table = document.getElementById("contactsTable");
+	  switching = true;
+	  
+	  while (switching) {
+		  switching = false;
+		  rows = table.rows;
+
+		  for (i = 0; i < (rows.length - 1); i++) {
+			  shouldSwitch = false;
+				x = rows[i].getElementsByTagName("td")[0];
+			  	y = rows[i + 1].getElementsByTagName("td")[0];
+			  
+			  if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+				shouldSwitch = true;
+				break;
+			  }
+		  }
+		  if (shouldSwitch) {
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+		  }
+	  }
+	}
