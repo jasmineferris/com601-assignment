ea8c787ad74eab8748b074ca587e1b38206b85f5
B00716443 <kelly-l53@ulster.ac.uk>
2019-12-10 13:01:29 +0000

    Cleaned up index.js
    Cleaned up index.js, added comments

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // Uses JSON Server module
+// https://github.com/typicode/json-server
+// http://zetcode.com/javascript/fakerjs/
+
+// Pulls in Node.JS modules and makes them required.
 const faker = require('faker');
 const fs = require('fs');
 
+// Faker doesn't have a predefined gender or sex methods, so this piece of code allows us to generate a random gender. 
 let genders = [ 'Male' , 'Female', 'Other' ];
 
+// Function where the fake contact data is generated.
 function generateContacts(){
+    // Declaring an array called 'users'.
     let users = [];
+    // For loops that generates 50 fake contact details and pushes it into variables.
     for (let id =1; id <=50; id++){
         let firstName = faker.name.firstName();
         let lastName = faker.name.lastName();
@@ -16,6 +24,7 @@ function generateContacts(){
         let post_code =faker.address.zipCode();
         let number = faker.phone.phoneNumber();
 
+        // Pushes the generated data in the variables into the 'users' array.
         users.push({
             "id": id,
             "first_name": firstName,
@@ -28,8 +37,11 @@ function generateContacts(){
             "number": number
         });
     }
+    // Returns the 'users' array within 'contacts'.
     return {"contacts": users}
 }
 
+// Declaring a variable called 'dataObj'. The 'generateContacts()' function is called and it's return data is placed into 'dataObj'.
 let dataObj = generateContacts();
+// Writes to the JSON file by placing the contents of 'dataObj' into it. 
 fs.writeFileSync('json/data.json', JSON.stringify(dataObj, null));
